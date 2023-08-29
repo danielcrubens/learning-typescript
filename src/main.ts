3f3908cdefacb1953684b44713b0ce00abe30a38
danielcrubens <dcr.tecnologi@gmail.com>
2023-08-29 10:06:12 -0300

    any, void, never  :memo:

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,17 @@ total = 10.5
 
 let isOpen: boolean 
 isOpen = false
+
+ //any
+ let stock: any = 89
+ stock = true 
+
+ // void
+function showInformation(name: string = "Daniel"):void{
+  console.log(name)
+}
+
+//never
+function error():never {
+  throw new Error("error"); 
+}
